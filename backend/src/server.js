06ad65a07fadb9a38f6fe203594d79eb347408b0
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2024-09-01 23:56:06 +1000

    setup mongodb and connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv').config();
 const app = express();
 const port = 3001;
+
+if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDb");
+    } catch (err) {
+        console.error(err);
+    }
+};
+connectDB();
 
 app.use("/", (req, res) => {
     res.send("Hello world");
