241ebeeb61ee4421d240033aed73ce88c25970a3
Daniel Abdelsayed <daniel.abdelsayed1@gmail.com>
2024-09-05 19:12:49 +1000

    added front-end dashboard components
    welcome, tasks, and weekly events

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Tile from '../components/Home/Tile';
+import Event from "../components/Home/Event";
 
 function Home() {
     let navigate = useNavigate();
@@ -15,15 +16,12 @@ function Home() {
     }, []);
 
     return (
-        <div className="w-full h-full px-8 text-black font-lexend">
-            <div class="flex justify-between h-10 mb-6">
-                <text className="text-4xl font-bold">Hello, {user.fullname}!</text>
-                <button className="bg-black text-xs text-white w-28 rounded-3xl">
-                    Your Review
-                </button>
+        <div className="w-full h-full text-black font-poppins">
+            <div class="flex justify-between h-10 mb-6 mx-8">
+                <text className="text-4xl font-bold font-lexend">Hello, {user.fullname}!</text>
             </div>
-            <div class="flex justify-between h-10 mb-6">
-                <button className="bg-secGrey text-xs w-28 rounded-3xl">
+            <div class="flex justify-between h-10 mb-6 mx-8">
+                <button className="bg-white text-xs w-28 rounded-3xl">
                     My Tasks
                 </button>
                 <button className="bg-secGrey text-xs w-28 rounded-3xl">
@@ -33,8 +31,20 @@ function Home() {
                     Completed
                 </button>
             </div>
-            <div className="flex bg-black/10 w-full h-56">
-                <Tile/>
+            <div className="flex flex-nowrap overflow-x-auto w-100vw h-56 mb-8">
+                <div className="flex flex-nowrap space-x-6 ml-8">
+                    <Tile />
+                    <Tile />
+                    <Tile />
+                </div>
+            </div>
+            <div className="mx-8">
+                <text className="text-xl font-semibold">Events this week</text>
+                <div className="flex flex-col space-y-4 mt-4">
+                    <Event />
+                    <Event />
+                    <Event />
+                </div>
             </div>
         </div>  
     )
