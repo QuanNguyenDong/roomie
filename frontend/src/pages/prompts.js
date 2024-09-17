d9b9cd67cd5029c98aec1c07074931337a77692f
ssumaiyaa <sumaiya.f.sidiqali@student.uts.edu.au>
2024-09-17 20:56:44 +1000

    Added prompts page

---
+++ frontend/src/pages/prompts.js
@@ -0,0 +1,10 @@
+const Prompts = () => {
+    return (
+      <div>
+        <h1>Prompts Page</h1>
+        <p>Select the questions you want to answer!</p>
+      </div>
+    );
+  };
+  
+  export default Prompts;
