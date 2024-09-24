bb855a40024bb17a3a82fd602500becd4741d270
trishmanjunath <108712861+trishmanjunath@users.noreply.github.com>
2024-09-24 19:49:53 +1000

    create task page commit

--- frontend/src/pages/CreateTask.js
+++ frontend/src/pages/CreateTask.js
@@ -6,16 +6,15 @@ import PeopleIcon from "../svgs/TaskManagement/PeopleIcon"
 const CreateTask = () => {
   return (
     <div className="modal-content">
-    <div className="relative pt-16 px-7 py-4 h-[40vh] bg-gradient-to-b from-[#7D8D9C] to-[#F6F6F6]">
-        <div className="header">
-        <div className='task-avatar2'><PeopleIcon/></div>
+    <div className="relative pt-16 px-7 py-4 h-[38vh] bg-gradient-to-b from-[#7D8D9C] to-[#F6F6F6]">
+        <div className='task-avatar3'><PeopleIcon/></div>
         <h1>Create Task</h1>
-        </div>
     </div>
     <h2>Description</h2>
     <div className='description'>
-      <p>hello</p>
+      <input type="text" id="description-task"></input>
       </div>
+      <button>Enter</button>
       </div>
   );
 };
